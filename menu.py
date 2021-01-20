b5d2e9d5cdd8e9d5a5c49160ac31029445ce99e7
6foot6Milos <75797883+6foot6Milos@users.noreply.github.com>
2021-01-19 22:09:12 -0800

    Testing the subprocess

--- menu.py
+++ menu.py
@@ -1,9 +1,23 @@
 from tkinter import *
 
 
+def game_start(): 
+    x=10
+    print(x)
+
+
 def start_menu():
     root = Tk()
     root.title("Puzzle-Hunter")
     root.geometry("1080x1080")
+
+
+
+
+
+    start_button = Button(root, text="Start Game", command = game_start)
+    start_button.grid(column=2,row=3)
+
+
 
     root.mainloop()
