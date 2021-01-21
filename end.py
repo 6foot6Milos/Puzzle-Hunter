63bb0adac725a2b4fe0ccdd6ec1678f9e18dd456
6foot6Milos <75797883+6foot6Milos@users.noreply.github.com>
2021-01-21 09:39:41 -0800

    Worked on start menu

--- end.py
+++ end.py
@@ -1,7 +1,14 @@
-from tkinter import *
-
-
+#Menu at the end of the game
 def play_again():
+    from tkinter import *
+    from main import window, pygame
+    pygame.init()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+
     root = Tk()
     root.title("Puzzle-Hunter")
     root.geometry("1080x1080")
@@ -9,6 +16,8 @@ def play_again():
 
     start_button = Button(root, text="Play Again",bg="Gold", command = root.destroy)
     start_button.place(bordermode=OUTSIDE, x=200, y=200)
+    window = 1
+    return window
 
     quit_button = Button(root, text="Quit", command = root.destroy)
     quit_button.place(bordermode=OUTSIDE, x=200, y=250)
--- menu.py
+++ menu.py
@@ -1,7 +1,12 @@
-from tkinter import *
-
-
+#Menu at the beginning of the game
 def start_menu():
+    from tkinter import *
+    from main import window, pygame
+    pygame.init()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
     root = Tk()
     root.title("Puzzle-Hunter")
     root.geometry("1080x1080")
@@ -9,10 +14,10 @@ def start_menu():
     
     
 
-
+    window = 1
     start_button = Button(root, text="Start Game", command = root.destroy)
     start_button.place(bordermode=OUTSIDE, x=200, y=200)
-
+    
 
 
     root.mainloop()
