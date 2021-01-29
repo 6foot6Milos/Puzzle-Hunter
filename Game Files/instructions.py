dd8220c4c913a1a0dcb64e75207cae0e8f36b70a
6foot6Milos <75797883+6foot6Milos@users.noreply.github.com>
2021-01-28 18:59:16 -0800

    Organised the instructions menu and the back button

--- Game Files/instructions.py
+++ Game Files/instructions.py
@@ -21,11 +21,11 @@ def instructions_menu():
     #pygame.mixer.music.load("Game Files/Objects/01_Main Menu.mp3")
     #pygame.mixer.music.play(loops=2)
 
-    #bg = PhotoImage(file="Game Files/Objects/Start Menu.png")
-    #bg_label = Label(root, image=bg)
-    #bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+    bg = PhotoImage(file="Game Files/Objects/Instructions Menu.png")
+    bg_label = Label(root, image=bg)
+    bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-    start_button = Button(root, text="Back", font=("Helvetica",32), bg = "cyan4",fg = "goldenrod2", width = 28, command = instructions_command)
-    start_button.place(bordermode=OUTSIDE, x=266, y=540)
+    start_button = Button(root, text="Back", font=("Helvetica",26), bg = "cyan4",fg = "goldenrod2", width = 17, command = instructions_command)
+    start_button.place(bordermode=OUTSIDE, x=482, y=250)
     
     root.mainloop()
